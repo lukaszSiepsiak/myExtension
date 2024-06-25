908aba7897c045517a2563bd2c541a007481498a
l.siepsiak <l.siepsiak@medidok.de>
2024-06-25 14:06:11 +0200

    comm

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,18 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "background.bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
