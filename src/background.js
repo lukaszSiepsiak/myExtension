4c5e48e61ba3034259ed276d97b2104bd63757f9
l.siepsiak <l.siepsiak@medidok.de>
2024-06-25 13:58:22 +0200

    Commit with webpack

---
+++ src/background.js
@@ -0,0 +1,75 @@
+import { XMLParser } from "fast-xml-parser";
+
+const EXTENSION_ID = "chibebbaajpcnjnnmacjngefcoginbhe";
+const EXTENSION_URL = "https://lukaszsiepsiak.github.io/myExtension/update.xml";
+const UPDATE_CHECK_INTERVAL = 60 * 60 * 1000; // Check every hour
+// background.js or service_worker.js
+chrome.runtime.onInstalled.addListener(() => {
+  console.log(`Extension with ID: ${EXTENSION_ID} Installed`);
+});
+
+// Function to check for updates
+async function checkForExtensionUpdate() {
+  console.log(`Checking for updates for extension with ID: ${EXTENSION_ID}`);
+
+  try {
+    const response = await fetch(EXTENSION_URL);
+    const text = await response.text();
+
+    // Parse the XML using fast-xml-parser
+    const parser = new XMLParser();
+    const result = parser.parse(text);
+    const latestVersion = result.gupdate.app.updatecheck["@_version"];
+
+    chrome.management.getSelf((extensionInfo) => {
+      const currentVersion = extensionInfo.version;
+      if (currentVersion !== latestVersion) {
+        notifyUserAboutUpdate(latestVersion);
+      }
+    });
+  } catch (error) {
+    console.error("Error checking for extension update:", error);
+  }
+}
+
+// Function to notify the user about the update
+function notifyUserAboutUpdate(latestVersion) {
+  chrome.notifications.create({
+    type: "basic",
+    iconUrl: "icons/icon48.png",
+    title: "Extension Update Available",
+    message: `A new version (${latestVersion}) of the extension is available.`,
+    buttons: [{ title: "Update Now" }],
+    priority: 0,
+  });
+
+  chrome.notifications.onButtonClicked.addListener(
+    (notificationId, buttonIndex) => {
+      if (buttonIndex === 0) {
+        chrome.runtime.reload();
+      }
+    }
+  );
+}
+
+// Check for updates periodically
+setInterval(checkForExtensionUpdate, UPDATE_CHECK_INTERVAL);
+checkForExtensionUpdate();
+
+// chrome.notifications.onButtonClicked.addListener(
+//   (notificationId, buttonIndex) => {
+//     if (buttonIndex === 0) {
+//       chrome.runtime.reload();
+//     }
+//   }
+// );
+
+// chrome.runtime.onStartup.addListener(() => checkForUpdate());
+// chrome.runtime.onInstalled.addListener(() => checkForUpdate());
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.message === "checkVersion") {
+//     checkForUpdate((updateAvailable) => sendResponse({ updateAvailable }));
+//     return true; // Keep the message channel open for sendResponse
+//   }
+// });
