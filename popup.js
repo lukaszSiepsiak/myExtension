9831720cdb885401024c7cafa3d7dfab7d27f2a9
l.siepsiak <l.siepsiak@medidok.de>
2024-07-01 14:19:01 +0200

    update new version

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
+document?.addEventListener("DOMContentLoaded", () => {
   const manifest = chrome.runtime.getManifest();
   document.getElementById("extension-name").innerText = manifest.name;
   document.getElementById("extension-version").innerText =
     "Version: " + manifest.version;
 });
 
-document.getElementById("updateButton").addEventListener("click", () => {
+document?.getElementById("updateButton")?.addEventListener("click", () => {
   chrome.runtime.sendMessage({ action: "checkForUpdate" });
 });
